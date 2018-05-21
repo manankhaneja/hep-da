c8acf9f5bf03d49382b068e1287db88c03d5aa02
Manan Khaneja <manankhaneja@yahoo.com>
2018-05-21 22:57:18 +0530

    revert

--- Programs/SCWext.py
+++ Programs/SCWext.py
@@ -4,19 +4,20 @@ Created on Wed May 16 18:08:30 2018
 
 @author: Manan Khaneja
 """
-
+#this program gives you the information about all the different types of particles, interaction types and detection heights present in a file
 import os
 user_input =input("Enter the path of your file: \n")
-col=int(input("1. Know all the particles \n2. Know all the interaction types\n"))
+col=int(input("1. Know all the particles \n2. Know all the interaction types\n3. Know all the detection heights\n"))
 assert os.path.isfile(user_input), "I did not find the file"
 print("\n")
 f= open(user_input, "r+")
-k=open('temp.txt','w')
 var_data= []
 if col==1:
-    col=3
+    col=3       #3 corresponds to the column with particle name
 elif col==2:
-    col=14
+    col=14      #14 corresponds to interaction type
+elif col==3:
+    col=12      #12 corresponds to all the detection heights 
 line=f.readline()
 while line:
     line=f.readline()
@@ -24,5 +25,9 @@ while line:
         line=f.readline()
     elif '###' in line:
         pass
-    else:
-        var_data.append((line.split(" ")[col]))
+    elif line.strip():
+            name= line.split(" ")[col]
+            if name not in var_data:
+                var_data.append(line.split(" ")[col])
+f.close()
+print(var_data)
