db6aa92b10fb351fd8d9e9c36a0c9db2c5720d1d
Manan Khaneja <manankhaneja@yahoo.com>
2018-06-13 01:22:01 +0530

    plot.py has been modified to be able to plot multiple files simultaneously.

--- programs/plot.py
+++ programs/plot.py
@@ -13,32 +13,42 @@ def plot():
 
     import os
     import matplotlib.pyplot as plt
-    import seaborn as sns
     ax = plt.axes()
     user_input =input("Enter the path of your directory: \n")
     assert os.path.isdir(user_input), "I did not find the directory"
     print("\n")
-    filename = input("Enter the filename \n")
-    file_path = os.path.join(user_input,filename)
-    plot_name = filename[0:len(filename)-4:1]
-    k = open(file_path, "r+")
-    var_data = []
-    col = int(input("Which variable you want to use (enter the column number choice): \n"))
-    for line in k:
-            var_data.append(float(line.split(" ")[col]))
+    index,flag = 1,1
+    plot_name = "plot"
+    var_data = [[],[],[],[],[]]             #can plot maximum 5 plots simultaneously
     bins = int(input("how many bins do you want to use? \n"))
     min = int(input("Enter the range integer -start value \n"))
     max = int(input("Enter the range integer -end value \n"))
-    (n, bins, patches) = plt.hist(var_data, bins, range= (min,max))
     xlabel = input("What is the x label \n")
     ylabel = input("What is the y label \n")
+    col = int(input("Which variable you want to use (enter the column number choice): \n"))
+    while flag:
+        filename = input("Enter the filename \n")
+        particle = input("Enter the associated particle \n")
+        file_path = os.path.join(user_input,filename)
+        plot_name = plot_name + '_' + filename[3:len(filename)-4:1]
+        k = open(file_path, "r+")
+        var_data[index] = []
+        for line in k:
+                var_data[index].append(float(line.split(" ")[col]))
+        (n, bins, patches) = plt.hist(var_data[index], bins, range = (min,max), histtype = 'step', alpha = 0.5, label = particle )
+        plt.legend(loc = 'upper right')
+        print("\n")
+        for i in range(len(bins)-1):
+            print("number ", n[i] , "bin ", bins[i] ,"\n")
+        flag = int(input("Do you want to give another file? \n1. Yes \n0.No \n"))
+        index = index + 1
+    plot_name = plot_name + '_' + xlabel
     ax.xaxis.set_major_locator(plt.MultipleLocator((max-min)/10))
     ax.xaxis.set_minor_locator(plt.MultipleLocator((max-min)/100))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(plot_name)
     plt.savefig(user_input + '/' + plot_name + '.png', bbox_inches='tight', dpi = 500)
-    for i in range(len(bins)-1):
-        print("number ", n[i] , "bin ", bins[i] ,"\n")
+
 
 plot()
